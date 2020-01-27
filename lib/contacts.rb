3731af5c4ead201103296229d7860689ba170d63
Lessllie Real <rlessllie@yahoo.com>
2020-01-27 22:35:49 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,18 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+contacts.each do |person, person_details|
+  if person == "Freddy Mercury"
+  person_details.each do |attribute, value|
+    
+    if attribute == :favorite_ice_cream_flavors
+  
+      value.each do |flavor|
+        value.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+       
+      end 
+      end 
+      end 
+      end 
 end
